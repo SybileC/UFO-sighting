fc89ca990d7cfcd25f7d148a199220fd8eaed7d4
SybileC <sybile.cherenfant@gmail.com>
2020-11-17 21:57:29 -0500

    Appended table to web page

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -7,6 +7,12 @@ tbody = d3.select("tbody");
 
 console.log(tableData)
 
-// tableData.forEach(info => {
-    
-// });
+tableData.forEach(info => {
+    console.log(info);
+    var row = tbody.append("tr");
+    Object.entries(info).forEach(([key, value]) => {
+        console.log(key, value);
+        cell = row.append("td");
+        cell.text(value);
+    });
+});
