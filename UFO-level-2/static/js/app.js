fa1e5754294118e8bac7c9f923e2e510d9086106
SybileC <sybile.cherenfant@gmail.com>
2020-11-21 23:00:45 -0500

    Added Level 2 folder

---
+++ UFO-level-2/static/js/app.js
@@ -0,0 +1,86 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+const tbody = d3.select("tbody");
+
+console.log(tableData)
+
+function createTable(tableData) {
+    // Clear existing table
+    tbody.html("")
+    
+    // Loop through tableData, append table and add rows to web page 
+    tableData.forEach(info => {
+    console.log(info);
+    const row = tbody.append("tr");
+    Object.values(info).forEach((value) => {
+        console.log(value);
+        let cell = row.append("td");
+        cell.text(value);
+    });
+});
+}
+
+// // var selInfo = d3.getElementById("selInfo");
+
+// //set up output string
+// var inputValue = d3.select("form>input").property("value");
+// // in += "<ul> n";
+// //step through options
+// for (i = 0; i < selInfo.length; i++){
+//  //examine current option
+//  currentOption = selLanguage[i];
+//  //print it if it has been selected
+//  if (currentOption.selected == true){
+//     if (inputValue); {
+//         console.log(d3.event.target);
+//         console.log(inputValue);
+//         filteredData = filteredData.filter(info => info.datetime === inputValue);
+
+//     createTable(filteredData);
+// //  result += " <li>" + currentOption.value + "</li> n";
+//  } // end if
+// } // end for loop
+
+// Create function to filter data based on input value
+function runEnter(){
+    d3.event.preventDefault();
+    
+    const inputValue = d3.select("form>input").property("value");
+
+    console.log(d3.event.target);
+    console.log(inputValue);
+
+    let filteredData = tableData
+
+    var selInfo = d3.getElementById("selInfo");
+
+    for (i = 0; i < selInfo.length; i++){
+        //examine current option
+        currentOption = selLanguage[i];
+        //print it if it has been selected
+        if (currentOption.selected == true){
+
+            if (inputValue); {
+        
+        filteredData = filteredData.filter(info => info.datetime === inputValue);
+
+    createTable(filteredData);
+}
+}
+
+}
+
+// Create Listening Event
+var button = d3.selectAll("#filter-btn");
+
+var form = d3.selectAll("form")
+
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Return to default table
+createTable(tableData);
